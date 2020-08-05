eeace30c8d666452f54aff6ccf39b523fb585bed
Jair Colina <jair.colina@gmail.com>
2020-08-04 23:50:07 -0300

    Segunda Miguation

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amaunt { get; set; }
+        public SaleStatus Stauts { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amaunt, SaleStatus stauts, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amaunt = amaunt;
+            Stauts = stauts;
+            Seller = seller;
+        }
+    }
+}
